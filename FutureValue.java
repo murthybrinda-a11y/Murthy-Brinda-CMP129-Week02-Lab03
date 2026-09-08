c2ee0f0db97e7ea2546f46861cb6b0bfcdf12968
murthy.brinda <murthy.brinda@student.ccm.edu>
2026-09-08 14:42:38 -0400

    Lab 03 almost done

--- FutureValue.java
+++ FutureValue.java
@@ -1,3 +1,26 @@
-public class FutureValue {
+import java.uitil.Scanner;
+
+public class FutureValue 
+{
+    public static void main (String[]args)
+
+    {
+        double PresentValue ;
+        double MonthlyinterestRate;
+        int NumMonths;
+
+
+        Scanner keyboard = new Scanner(System.in);
+        System.out.print("Please enter the accounts present value: ");
+        PresentValue = keyboard.nextDouble();
+        System.out.print("Enter the Monthly interest rate: ");
+        MonthlyinterestRate = keyboard.nextDouble();
+
+
+
+
+
+
+    }
     
 }
