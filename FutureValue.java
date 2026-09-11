7cf6e09978fb3df3652d5a1cd9e0b88bc2046a7d
murthy.brinda <murthy.brinda@student.ccm.edu>
2026-09-11 14:30:10 -0400

    Lab 03 almost done

--- FutureValue.java
+++ FutureValue.java
@@ -1,26 +1,37 @@
-import java.uitil.Scanner;
+import java.util.Scanner;
 
 public class FutureValue 
 {
     public static void main (String[]args)
 
     {
-        double PresentValue ;
+        double PresentValue;
         double MonthlyinterestRate;
         int NumMonths;
-
+        double FutureValue;
+        double finalfuturevalue; // to store the return 
+        finalfuturevalue = FutureValue( double pv,double ir, double nm )
 
         Scanner keyboard = new Scanner(System.in);
+
         System.out.print("Please enter the accounts present value: ");
         PresentValue = keyboard.nextDouble();
+
         System.out.print("Enter the Monthly interest rate: ");
         MonthlyinterestRate = keyboard.nextDouble();
 
-
-
-
-
+        System.out.println("Enter the number of months: ");
+        NumMonths = keyboard.nextInt();     
 
     }
+    
+        public static double futureValue(double PresentValue, int NMonths,double mir)
+        {
+            System.out.println("The accounts Future Value is : " + "futurevalue");
+
+            double finalfuturevalue = PresentValue * Math.pow (1 + MonthlyinterestRate,2);
+            return finalfuturevalue;
+
+        }
     
 }
--- KineticEnergy.java
+++ KineticEnergy.java
@@ -1,3 +1,32 @@
-public class KineticEnergy {
+import java.util.Scanner;
+
+public class KineticEnergy 
+{
+    public static void main(String[] args) 
+    {
+        Scanner keyboard = new Scanner(System.in);
+        double KineticEnergyResult ;
+        double m;
+        double v;
+
+        System.out.println("Enter the mass of the object (in kg) : ");
+        m = keyboard.nextDouble();
+        
+        System.out.println ("Enter the velocity of the object (in m/s): ");
+        v = keyboard.nextDouble();
+
+        KineticEnergyResult = kineticEnergy( m,  v);
+        System.out.println(" The objects kinetic Energy is " + KineticEnergyResult + "J");
+
+        
+        
+    }
+    public static double kineticEnergy(double mass, double velocity)
+    {
+
+        double KineticEnergyResult = 0.5* mass* Math.pow(velocity,2);
+        return KineticEnergyResult;
+
+    }
 
 }
