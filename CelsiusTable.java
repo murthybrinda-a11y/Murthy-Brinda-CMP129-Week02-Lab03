df55071eeea85509bb4794f7974335c8781af3fb
murthy.brinda <murthy.brinda@student.ccm.edu>
2026-09-15 01:06:36 -0400

    Lab 03 celcius table code

--- CelsiusTable.java
+++ CelsiusTable.java
@@ -1,3 +1,24 @@
-public class CelsiusTable {
+public class CelsiusTable 
+{
+        public static void main(String[] args)
+         { 
+            double celsiusTemp; 
+        
+        System.out.println("Fahrenheit\tCelsius"); 
+        System.out.println("------------------------"); 
+        
+        for (int fahrenheit = 0; fahrenheit <= 20; fahrenheit++) 
+            { 
+                celsiusTemp = celsius(fahrenheit); 
+                System.out.printf("%d\t\t%.2f%n", fahrenheit, celsiusTemp); 
+            } 
+    } 
     
+    public static double celsius(double fahrenheit)
+     { 
+        double result = (5.0 / 9.0) * (fahrenheit - 32.0); 
+        return result; 
+     } 
 }
+    
+
