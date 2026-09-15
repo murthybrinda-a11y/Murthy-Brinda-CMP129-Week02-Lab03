c19e0030e788f9c4ecc1442ab75fc0171e76fb89
murthy.brinda <murthy.brinda@student.ccm.edu>
2026-09-15 00:56:52 -0400

    Lab 03 future value code

--- FutureValue.java
+++ FutureValue.java
@@ -1,37 +1,30 @@
-import java.util.Scanner;
+import java.util.Scanner; 
 
-public class FutureValue 
-{
-    public static void main (String[]args)
-
-    {
-        double PresentValue;
-        double MonthlyinterestRate;
-        int NumMonths;
-        double FutureValue;
-        double finalfuturevalue; // to store the return 
-        finalfuturevalue = FutureValue( double pv,double ir, double nm )
-
-        Scanner keyboard = new Scanner(System.in);
-
-        System.out.print("Please enter the accounts present value: ");
-        PresentValue = keyboard.nextDouble();
-
-        System.out.print("Enter the Monthly interest rate: ");
-        MonthlyinterestRate = keyboard.nextDouble();
-
-        System.out.println("Enter the number of months: ");
-        NumMonths = keyboard.nextInt();     
-
-    }
+public class FutureValue { 
+    public static void main (String[] args) { 
+        double presentValue; 
+        double monthlyInterestRate; 
+        int numMonths; 
+        double finalFutureValue; 
+        
+        Scanner keyboard = new Scanner(System.in); 
+        
+        System.out.print("Please enter the accounts present value: "); 
+        presentValue = keyboard.nextDouble(); 
+        
+        System.out.print("Enter the Monthly interest rate: "); 
+        monthlyInterestRate = keyboard.nextDouble(); 
+        
+        System.out.print("Enter the number of months: "); 
+        numMonths = keyboard.nextInt(); 
+        
+        finalFutureValue = futureValue(presentValue, numMonths, monthlyInterestRate); 
+        
+        System.out.printf("The account's Future Value is: $%.2f%n", finalFutureValue);
+    } 
     
-        public static double futureValue(double PresentValue, int NMonths,double mir)
-        {
-            System.out.println("The accounts Future Value is : " + "futurevalue");
-
-            double finalfuturevalue = PresentValue * Math.pow (1 + MonthlyinterestRate,2);
-            return finalfuturevalue;
-
-        }
-    
+    public static double futureValue(double presentValue, int nMonths, double mir) { 
+        double result = presentValue * Math.pow(1 + mir, nMonths); 
+        return result; 
+    } 
 }
